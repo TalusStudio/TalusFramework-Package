e2751f85b7f44acdb60f4781f6f5e3e9480d068e
Emre - Win <kovanciemre@gmail.com>
2022-01-03 16:54:27 +0300

    Add collection utility.

--- Runtime/Collections/FloatCollectionSO.cs
+++ Runtime/Collections/FloatCollectionSO.cs
@@ -7,5 +7,13 @@ namespace TalusFramework.Runtime.Collections
 {
     [CreateAssetMenu(fileName = "New Float Variable Collection", menuName = "Collections/Float Variable", order = 4)]
     public class FloatCollectionSO : BaseCollectionSO<FloatVariableSO>
-    { }
+    {
+        public void ResetAllValues(float value)
+        {
+            for (int i = 0; i < Items.Count; ++i)
+            {
+                Items[i].SetValue(value);
+            }
+        }
+    }
 }
--- Runtime/Collections/IntCollectionSO.cs
+++ Runtime/Collections/IntCollectionSO.cs
@@ -7,5 +7,13 @@ namespace TalusFramework.Runtime.Collections
 {
     [CreateAssetMenu(fileName = "New Int Variable Collection", menuName = "Collections/Int Variable", order = 3)]
     public class IntCollectionSO : BaseCollectionSO<IntVariableSO>
-    { }
+    {
+        public void ResetAllValues(int value)
+        {
+            for (int i = 0; i < Items.Count; ++i)
+            {
+                Items[i].SetValue(value);
+            }
+        }
+    }
 }
