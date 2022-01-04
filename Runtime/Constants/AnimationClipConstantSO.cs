575d11970014d4a699c5b76edd61cc0d351930c1
Emre - Win <kovanciemre@gmail.com>
2022-01-04 09:44:06 +0300

    Add AnimationClipConstant

---
+++ Runtime/Constants/AnimationClipConstantSO.cs
@@ -0,0 +1,10 @@
+﻿using TalusFramework.Runtime.Constants.Interfaces;
+
+using UnityEngine;
+
+namespace TalusFramework.Runtime.Constants
+{
+    [CreateAssetMenu(fileName = "New Animation Clip Constant", menuName = "Constants/Animation Clip", order = 8)]
+    public sealed class AnimationClipConstantSO : BaseConstantSO<AnimationClip>
+    { }
+}
