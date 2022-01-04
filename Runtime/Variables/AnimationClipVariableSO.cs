9a4f96ec4ffc43799dcbe16d4a3442589083985e
Emre - Win <kovanciemre@gmail.com>
2022-01-04 09:34:05 +0300

    Add AnimationClipVariable

---
+++ Runtime/Variables/AnimationClipVariableSO.cs
@@ -0,0 +1,10 @@
+﻿using TalusFramework.Runtime.Variables.Interfaces;
+
+using UnityEngine;
+
+namespace TalusFramework.Runtime.Variables
+{
+    [CreateAssetMenu(fileName = "New Animation Clip Variable", menuName = "Variables/Animation Clip", order = 9)]
+    public sealed class AnimationClipVariableSO : BaseVariableSO<AnimationClip>
+    { }
+}
