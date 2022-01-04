89ccb9aa6575c34d1a1eb698c3d9d5fd2af4347c
Emre - Win <kovanciemre@gmail.com>
2022-01-04 09:24:49 +0300

    Add AnimationClipReference

---
+++ Runtime/References/AnimationClipReference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using TalusFramework.Runtime.References.Interfaces;
+
+namespace TalusFramework.Runtime.References
+{
+    [Serializable]
+    public sealed class AnimationClipReference : BaseReference<UnityEngine.AnimationClip>
+    {
+        public static implicit operator UnityEngine.AnimationClip(AnimationClipReference reference) => reference.Value;
+    }
+}
