1ebb792f84487e4dca621d8d8b6f35d441f58fba
Emre - Win <kovanciemre@gmail.com>
2022-02-02 00:14:34 +0300

    Update TimeScaleManager

--- Runtime/Managers/TimeScaleManager.cs
+++ Runtime/Managers/TimeScaleManager.cs
@@ -9,12 +9,10 @@ namespace TalusFramework.Runtime.Managers
     [CreateAssetMenu(fileName = "New TimeScale Manager", menuName = "Managers/TimeScale Manager", order = 2)]
     public class TimeScaleManager : BaseSO
     {
-        [Button(ButtonSizes.Large), GUIColor(1f, 1f, 0f)]
-        [EnableIf("@Time.timeScale != 0f")]
-        public void PauseGame() => Time.timeScale = 0f;
-
-        [Button(ButtonSizes.Large), GUIColor(0f, 1f, 0f)]
-        [EnableIf("@Time.timeScale == 0f")]
-        public void UnpauseGame() => Time.timeScale = 1f;
+        [Button]
+        public void SetTimeScale(float timeScale)
+        {
+            Time.timeScale = timeScale;
+        }
     }
 }
