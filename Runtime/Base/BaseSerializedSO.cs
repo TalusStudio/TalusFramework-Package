1aad3b21090ed9e7cd179f5fbb93c64365e7f12b
Emre - Win <kovanciemre@gmail.com>
2022-02-03 14:32:19 +0300

    Add BaseSerializedSO class

---
+++ Runtime/Base/BaseSerializedSO.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.OdinInspector;
+
+using UnityEngine;
+
+namespace TalusFramework.Runtime.Base
+{
+    /// <summary>
+    ///     Base class for Odin Serialized Scriptable Objects in Talus Framework.
+    /// </summary>
+    [HideMonoScript]
+    public abstract class BaseSerializedSO : SerializedScriptableObject
+    {
+#pragma warning disable 0414
+        [HideLabel, PropertyOrder(0), FoldoutGroup("Developer Description")]
+        [TextArea(5, 10)]
+        [SerializeField]
+        private string _DeveloperDescription = "";
+#pragma warning restore 0414
+    }
+}
--- Runtime/Managers/GameManager.cs
+++ Runtime/Managers/GameManager.cs
@@ -11,15 +11,8 @@ namespace TalusFramework.Runtime.Managers
 {
     [HideMonoScript]
     [CreateAssetMenu(fileName = "New Game Manager", menuName = "Managers/Game Manager", order = 0)]
-    public class GameManager : SerializedScriptableObject, IInitializable
+    public class GameManager : BaseSerializedSO, IInitializable
     {
-#pragma warning disable 0414
-        [HideLabel, PropertyOrder(0), FoldoutGroup("Developer Description")]
-        [TextArea(5, 10)]
-        [SerializeField]
-        private string _DeveloperDescription = "";
-#pragma warning restore 0414
-
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Init()
         {
