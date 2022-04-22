490848b8e4a742dfce96c7b1dfac3328f58007d0
Emre Kovanci <kovanciemre@gmail.com>
2022-04-22 23:03:05 +0300

    update debug manager

--- Runtime/Managers/RuntimeDebugManager.cs
+++ Runtime/Managers/RuntimeDebugManager.cs
@@ -19,6 +19,26 @@ namespace TalusFramework.Runtime.Managers
     [CreateAssetMenu(fileName = "New Runtime Debug Manager", menuName = "Managers/Runtime Debug Manager", order = 2)]
     public class RuntimeDebugManager : BaseSO, IInitializable
     {
+#if UNITY_EDITOR
+        private bool IsCommandsActivated()
+        {
+            #if ENABLE_COMMANDS
+            return true;
+            #else
+            return false;
+            #endif
+        }
+
+        [ShowIf("@IsCommandsActivated() == false")]
+        [Title("Command definition can not found!")]
+        [GUIColor(0f, 1f, 0f)]
+        [Button(ButtonSizes.Large)]
+        public void AddCommandDefinition()
+        {
+
+        }
+#endif
+
         [ToggleLeft]
         [SerializeField]
         private bool _EnableHiddenDebugView;
