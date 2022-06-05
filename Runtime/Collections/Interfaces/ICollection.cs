d649d3c212e7852c766d3ea28ec5d59f794876fb
Emre Kovanci <kovanciemre@gmail.com>
2022-06-06 02:46:48 +0300

    update collection interface

--- Runtime/Collections/Interfaces/ICollection.cs
+++ Runtime/Collections/Interfaces/ICollection.cs
@@ -3,12 +3,17 @@ namespace TalusFramework.Collections.Interfaces
     public interface ICollection
     {
         int Count { get; }
+
+        void Clear();
     }
 
     public interface ICollection<T> : ICollection
     {
+        public T this[int index] { get; set; }
+
         bool Add(T thing);
         bool Remove(T thing);
         void ForEach(System.Action<T> action);
+
     }
 }
