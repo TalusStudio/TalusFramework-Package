6b276a1cb7a6eadfe558eb5a08165c89772cdd2f
Emre Kovanci <kovanciemre@gmail.com>
2022-06-12 18:50:43 +0300

    refactor property drawers

--- Editor/PropertyDrawers/BaseReferenceDrawer.cs
+++ Editor/PropertyDrawers/BaseReferenceDrawer.cs
@@ -1,75 +1,61 @@
-using TalusFramework.References.Interfaces;
-
 using UnityEditor;
-
 using UnityEngine;
+
+using TalusFramework.References.Interfaces;
 
 namespace TalusFramework.Editor.PropertyDrawers
 {
     [CustomPropertyDrawer(typeof(BaseReference))]
     public class BaseReferenceDrawer : PropertyDrawer
     {
-        /// <summary>
-        ///     Options to display in the popup to select constant or variable.
-        /// </summary>
-        private readonly string[] popupOptions =
-        {
-            "Use Variable",
-            "Use Constant"
-        };
-
-        /// <summary>
-        ///     Cached style to use to draw the popup button.
-        /// </summary>
-        private GUIStyle popupStyle;
+        private readonly string[] _PopupOptions = { "Use Variable", "Use Constant" };
+        private GUIStyle _PopupStyle;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (popupStyle == null)
+            if (_PopupStyle == null)
             {
-                popupStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
-                popupStyle.imagePosition = ImagePosition.ImageOnly;
+                _PopupStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
+                _PopupStyle.imagePosition = ImagePosition.ImageOnly;
             }
 
-            // Store old indent level and set it to 0, the PrefixLabel takes care of it
+            //
             int indent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
 
-            // Get condition properties
+            //
             SerializedProperty useConstant = property.FindPropertyRelative("UseConstant");
-
-            // Get values
             SerializedProperty constantValue = property.FindPropertyRelative("ConstantValue");
             SerializedProperty variableValue = property.FindPropertyRelative("Variable");
 
             label = EditorGUI.BeginProperty(position, label, property);
-            //todo: refactor comment
-            //label.text += "(" + (useConstant.boolValue ? "Const" : "Variable") + ")";
-            position = EditorGUI.PrefixLabel(position, label);
-
-            EditorGUI.BeginChangeCheck();
-
-            // Calculate rect for configuration button
-            var buttonRect = new Rect(position);
-            buttonRect.yMin += popupStyle.margin.top;
-            buttonRect.width = popupStyle.fixedWidth + popupStyle.margin.right;
-            buttonRect.height = popupStyle.fixedHeight + popupStyle.margin.top;
-            position.xMin = buttonRect.xMax;
-
-            int index = EditorGUI.Popup(buttonRect, useConstant.boolValue ? 1 : 0, popupOptions, popupStyle);
-            useConstant.boolValue = index == 1;
-
-            EditorGUI.PropertyField(position, useConstant.boolValue ? constantValue : variableValue, GUIContent.none);
-
-            if (EditorGUI.EndChangeCheck())
             {
-                if (property.serializedObject.hasModifiedProperties)
-                {
-                    property.serializedObject.ApplyModifiedProperties();
-                }
-            }
+                position = EditorGUI.PrefixLabel(position, label);
 
-            EditorGUI.indentLevel = indent;
+                EditorGUI.BeginChangeCheck();
+
+                // Calculate rect for configuration button
+                var popupRect = new Rect(position);
+                popupRect.yMin += _PopupStyle.margin.top;
+                popupRect.width = _PopupStyle.fixedWidth + _PopupStyle.margin.right;
+                popupRect.height = _PopupStyle.fixedHeight + _PopupStyle.margin.top;
+                position.xMin = popupRect.xMax;
+
+                int index = EditorGUI.Popup(popupRect, useConstant.boolValue ? 1 : 0, _PopupOptions, _PopupStyle);
+                useConstant.boolValue = index == 1;
+
+                EditorGUI.PropertyField(position, useConstant.boolValue ? constantValue : variableValue, GUIContent.none);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    if (property.serializedObject.hasModifiedProperties)
+                    {
+                        property.serializedObject.ApplyModifiedProperties();
+                    }
+                }
+
+                EditorGUI.indentLevel = indent;
+            }
             EditorGUI.EndProperty();
         }
     }
--- Editor/PropertyDrawers/SceneReferenceDrawer.cs
+++ Editor/PropertyDrawers/SceneReferenceDrawer.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Linq;
-
 using UnityEditor;
 using UnityEngine;
 
@@ -17,75 +14,25 @@ namespace TalusFramework.Editor.PropertyDrawers
             var isDirtyProperty = property.FindPropertyRelative("m_IsDirty");
             if (isDirtyProperty.boolValue)
             {
-                isDirtyProperty.boolValue = false;
                 // This will force change in the property and make it dirty.
                 // After the user saves, he'll actually see the changed changes and commit them.
+                isDirtyProperty.boolValue = false;
             }
 
             EditorGUI.BeginProperty(position, label, property);
-            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-            /*const float buildSettingsWidth = 20f;
-            const float padding = 2f;
-
-            Rect assetPos = position;
-            assetPos.width -= buildSettingsWidth + padding;
-
-            Rect buildSettingsPos = position;
-            buildSettingsPos.x += position.width - buildSettingsWidth + padding;
-            buildSettingsPos.width = buildSettingsWidth;*/
-
-            var sceneAssetProperty = property.FindPropertyRelative("m_SceneAsset");
-            bool hadReference = sceneAssetProperty.objectReferenceValue != null;
-
-            EditorGUI.PropertyField(position, sceneAssetProperty, new GUIContent());
-
-            string guid = string.Empty;
-            int indexInSettings = -1;
-
-            if (sceneAssetProperty.objectReferenceValue)
             {
-                long localId;
-                if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(sceneAssetProperty.objectReferenceValue, out guid, out localId))
+                position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+                var sceneAssetProperty = property.FindPropertyRelative("m_SceneAsset");
+                bool hadReference = sceneAssetProperty.objectReferenceValue != null;
+
+                EditorGUI.PropertyField(position, sceneAssetProperty, new GUIContent());
+
+                if (sceneAssetProperty.objectReferenceValue == null && hadReference)
                 {
-                    indexInSettings = Array.FindIndex(EditorBuildSettings.scenes, s => s.guid.ToString() == guid);
+                    property.FindPropertyRelative("m_ScenePath").stringValue = string.Empty;
                 }
             }
-            else if (hadReference)
-            {
-                property.FindPropertyRelative("m_ScenePath").stringValue = string.Empty;
-            }
-
-            /// disabled...
-            /*GUIContent settingsContent = indexInSettings != -1
-                ? new GUIContent("-", "Scene is already in the Editor Build Settings. Click here to remove it.")
-                : new GUIContent("+", "Scene is missing in the Editor Build Settings. Click here to add it.")
-                ;
-
-            Color prevBackgroundColor = GUI.backgroundColor;
-            GUI.backgroundColor = indexInSettings != -1 ? Color.red : Color.green;
-
-            if (GUI.Button(buildSettingsPos, settingsContent, EditorStyles.miniButtonRight) && sceneAssetProperty.objectReferenceValue)
-            {
-                if (indexInSettings != -1)
-                {
-                    var scenes = EditorBuildSettings.scenes.ToList();
-                    scenes.RemoveAt(indexInSettings);
-
-                    EditorBuildSettings.scenes = scenes.ToArray();
-
-                }
-                else
-                {
-                    var newScenes = new EditorBuildSettingsScene[] {
-                        new EditorBuildSettingsScene(AssetDatabase.GetAssetPath(sceneAssetProperty.objectReferenceValue), true)
-                    };
-
-                    EditorBuildSettings.scenes = EditorBuildSettings.scenes.Concat(newScenes).ToArray();
-                }
-            }
-
-            GUI.backgroundColor = prevBackgroundColor;*/
 
             EditorGUI.EndProperty();
         }
