3cde2f6c24374c2e787f67d6105f993228bb696f
Emre Kovanci <kovanciemre@gmail.com>
2022-06-13 22:13:50 +0300

    remove indent

--- Runtime/Events/AnimationClipEvent.cs
+++ Runtime/Events/AnimationClipEvent.cs
@@ -6,7 +6,5 @@ namespace TalusFramework.Events
 {
     [CreateAssetMenu(fileName = "New Animation Clip Event", menuName = "Events/Animation Clip", order = 8)]
     public class AnimationClipEvent : BaseGameEvent<AnimationClip>
-    {
-
-    }
+    { }
 }
