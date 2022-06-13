5512ce70559d53c729c9beae01ea1c019a82ff9e
Emre Kovanci <kovanciemre@gmail.com>
2022-06-14 01:56:33 +0300

    update scene loader

--- Runtime/SceneManagement/SceneLoader.cs
+++ Runtime/SceneManagement/SceneLoader.cs
@@ -8,12 +8,21 @@ using Sirenix.OdinInspector;
 using TalusFramework.Base;
 using TalusFramework.Utility;
 using TalusFramework.Utility.Assertions;
+using TalusFramework.Variables;
 
 namespace TalusFramework.SceneManagement
 {
     [CreateAssetMenu(fileName = "New Scene Loader", menuName = "Others/Scene Loader", order = 1)]
     public class SceneLoader : BaseSO
     {
+        [Button, DisableInEditorMode]
+        public async void LoadLevel(SceneVariable scene)
+        {
+            this.Assert(!scene.RuntimeValue.IsEmpty, "There is an invalid scene reference!");
+
+            await LoadScene(scene.RuntimeValue.ScenePath);
+        }
+
         [Button, DisableInEditorMode]
         public async void LoadLevel(SceneReference scene)
         {
