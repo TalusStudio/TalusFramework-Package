96212714fda83887c072998faa04612e131e8e58
Emre Kovanci <kovanciemre@gmail.com>
2022-06-15 17:16:45 +0300

    update attribute

--- Runtime/Systems/Interfaces/AbstractFactory.cs
+++ Runtime/Systems/Interfaces/AbstractFactory.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
+using Sirenix.OdinInspector;
+
 using TalusFramework.Base;
 
 namespace TalusFramework.Systems.Interfaces
 {
     public abstract class AbstractFactory<T> : BaseSO
     {
-        [SerializeField]
+        [LabelWidth(50)]
+        [Required, SerializeField]
         private T _Object;
         public T Object => _Object;
 
