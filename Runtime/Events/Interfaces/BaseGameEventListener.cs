bf3b7adca71d5866ae57ef35de530919f7fccb1c
Emre Kovanci <kovanciemre@gmail.com>
2022-06-15 15:15:02 +0300

    update assertions

--- Runtime/Events/Interfaces/BaseGameEventListener.cs
+++ Runtime/Events/Interfaces/BaseGameEventListener.cs
@@ -7,6 +7,9 @@ using TalusFramework.Utility.Assertions;
 
 namespace TalusFramework.Events.Interfaces
 {
+    /// <summary>
+    ///     Base: Void Event
+    /// </summary>
     [HideMonoScript]
     public abstract class BaseGameEventListener : MonoBehaviour, IGameEventListener
     {
@@ -21,22 +24,28 @@ namespace TalusFramework.Events.Interfaces
         public void Send()
         {
             this.Assert(EventHelper.IsValidEvent(Response) == true, "There is a broken target on event!");
+
             Response?.Invoke();
         }
 
         protected virtual void OnEnable()
         {
-            this.Assert(GameEvent != null, "GameEvent reference is null!");
+            this.Assert(GameEvent != null, "Invalid Reference!", typeof(VoidEvent), null);
+
             GameEvent.AddListener(this);
         }
 
         protected virtual void OnDisable()
         {
-            this.Assert(GameEvent != null, "GameEvent reference is null!");
+            this.Assert(GameEvent != null, "Invalid Reference!", typeof(VoidEvent), null);
+
             GameEvent.RemoveListener(this);
         }
     }
 
+    /// <summary>
+    ///     Base: Generic Event
+    /// </summary>
     [HideMonoScript]
     public abstract class BaseGameEventListener<TPlainType, TEventType> : MonoBehaviour, IGameEventListener<TPlainType>
         where TEventType : IGameEvent<TPlainType>
@@ -53,18 +62,21 @@ namespace TalusFramework.Events.Interfaces
         public void Send(TPlainType param)
         {
             this.Assert(EventHelper.IsValidEvent(Response) == true, "There is a broken target on event!");
+
             Response?.Invoke(param);
         }
 
         protected virtual void OnEnable()
         {
-            this.Assert(GameEvent != null, "GameEvent reference is null!");
+            this.Assert(GameEvent != null, "Invalid Reference!", typeof(TEventType), null);
+
             GameEvent.AddListener(this);
         }
 
         protected virtual void OnDisable()
         {
-            this.Assert(GameEvent != null, "GameEvent reference is null!");
+            this.Assert(GameEvent != null, "Invalid Reference!", typeof(TEventType), null);
+
             GameEvent.RemoveListener(this);
         }
     }
