4c156d6ea5076a635012f09ae72119fc8539c2aa
Emre Kovanci <kovanciemre@gmail.com>
2022-06-21 02:34:42 +0300

    refactor gameobject variable

--- Runtime/Variables/GameObjectVariable.cs
+++ Runtime/Variables/GameObjectVariable.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 
-using TalusFramework.Base;
 using TalusFramework.Variables.Interfaces;
 
 namespace TalusFramework.Variables
@@ -9,16 +8,5 @@ namespace TalusFramework.Variables
     public sealed class GameObjectVariable : BaseVariable<GameObject>
     {
         public override bool AreValuesEqual(GameObject value) => ReferenceEquals(RuntimeValue, value);
-
-        public override void SetValue(GameObject value)
-        {
-            base.SetValue(value);
-        }
-
-        public override void SetValue(BaseValue value)
-        {
-            var variable = value as BaseValue<GameObject>;
-            base.SetValue(variable.RuntimeValue);
-        }
     }
 }
