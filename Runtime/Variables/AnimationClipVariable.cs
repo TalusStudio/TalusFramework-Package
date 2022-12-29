8e9e02feaa9cde7258018ca449f4982cf1f91d02
Emre Kovanci <kovanciemre@gmail.com>
2022-12-30 00:55:00 +0300

    add missing funcs

--- Runtime/Variables/AnimationClipVariable.cs
+++ Runtime/Variables/AnimationClipVariable.cs
@@ -6,5 +6,7 @@ namespace TalusFramework.Variables
 {
     [CreateAssetMenu(fileName = "New Animation Clip Variable", menuName = "Variables/Animation Clip", order = 8)]
     public sealed class AnimationClipVariable : BaseVariable<AnimationClip>
-    { }
+    {
+        public override bool AreValuesEqual(AnimationClip value) => ReferenceEquals(RuntimeValue, value);
+    }
 }
--- Runtime/Variables/SpriteVariable.cs
+++ Runtime/Variables/SpriteVariable.cs
@@ -6,5 +6,7 @@ namespace TalusFramework.Variables
 {
     [CreateAssetMenu(fileName = "New Sprite Variable", menuName = "Variables/Sprite", order = 7)]
     public sealed class SpriteVariable : BaseVariable<Sprite>
-    { }
+    {
+        public override bool AreValuesEqual(Sprite value) => ReferenceEquals(RuntimeValue, value);
+    }
 }
